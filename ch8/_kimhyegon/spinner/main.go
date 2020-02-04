81a9ed66ec14e59b2f8651e1e7029eb3a9ae90ab
Blackwoolf <mud0107@chol.com>
2020-02-05 07:51:53 +0900

    코드 업ㄷ

---
+++ ch8/_kimhyegon/spinner/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+/*
+	8.1 고루틴은 호출 함수 앞에 go 키워드 사용.
+	예)
+		f() // 일반 함수 호출
+		go f() 고루틴
+		동시성과 병렬성의 차이
+		동시성은 코드이고 병렬성의 실제 물리적이 코어에 실행된는 것
+*/
+
+func main() {
+	go spinner(100 * time.Millisecond)
+	const n = 45
+	fibN := fib(n)
+	fmt.Printf("\rFibonacci(%d) = %d\n", n, fibN)
+}
+
+func spinner(delay time.Duration) {
+	for {
+		for _, r := range `-\|/` {
+			fmt.Printf("\r%c", r)
+			time.Sleep(delay)
+		}
+	}
+}
+
+func fib(x int) int {
+	if x < 2 {
+		return x
+	}
+
+	return fib(x-1) + fib(x-2)
+}
